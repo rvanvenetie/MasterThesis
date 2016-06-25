bc2213253d57a192dedf5a7ff3d0fe6f1e8459b6
Jan Westerdiep <janner@gmail.com>
2016-06-25 14:09:05 +0200

    Updated image conversion script

--- Code/ifem/imgconver.m
+++ Code/ifem/imgconver.m
@@ -6,9 +6,16 @@ function imconver
     if strcmp(file, '')
       continue
     end
+    % Delete all bullshit files
+    delete(strrep(file, '.fig', '.png'), strrep(file, '.fig', '.eps'), strrep(file, '.fig', '.pdf'));
+    delete(strrep(file, '.fig', '_ef.png'), strrep(file, '.fig', '_ef.eps'), strrep(file, '.fig', '_ef.pdf'));
+    warning('off','last');
+
+    % Do not open this figure
     figure('visible' ,'off')
     fig = openfig(file, 'invisible');
-    export_fig(fig, strrep(file, '.fig' ,'_ef'),'-pdf', '-png', '-eps', '-painters');
+    set(fig, 'Color', 'none'); % Set background transparent
+    export_fig(fig, strrep(file, '.fig' ,''),'-pdf', '-painters'); % Export to PDF using export_fig
     %saveas(fig, strrep(file, '.fig', '.eps'))
   end
 end
