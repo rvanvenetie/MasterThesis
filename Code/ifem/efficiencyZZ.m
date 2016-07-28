385a98b4926c2deab71a3019b18dc6259c54661d
Raymond van Venetie <rayvanve@gmail.com>
2016-07-28 14:27:33 +0200

    working example

--- Code/ifem/efficiencyZZ.m
+++ Code/ifem/efficiencyZZ.m
@@ -1,27 +1,28 @@
 function efficiencyZZ
-  maxN = 2e5
+  maxN = 2e3
   meths = {'square_sin', 'square_ana', 'lshape_one' , 'crack_one'};
   colors = {'r', 'b', [0 0.5 0], 'm'};
   markers = ['o','x','s','d'];
   handles = [0,0,0,0];
   fig = figure(6); clf;
   for i=1:4
-    figure(1);
     % Retreive the example data
-    [node, elem, pde, bdFlag] = example(meths{i});
+    [node, elem, bdFlag, pde] = examples(meths{i});
 
     % Calculate the exact estimator
     [N, errH1] = estimateuniform('exact', node, elem, pde, bdFlag, maxN);
     % Calculate the ZZ estimator
     [N, errZZ] = estimateuniform('zz', node, elem, pde, bdFlag, maxN);
-
+    
+    figure(fig);
     loglog(N, errH1 ./ errZZ, '-', 'color', colors{i}, 'marker', markers(i)); hold on;
   end
+  figure(fig); hold on;
   title('Efficiency index comparison');
   xlabel('Number of vertices');
   legend({'Square (sin)', 'Square (poly)', 'L-shape (one)', 'Crack (one)'}, 'location', 'best');
 
-  saveas(gca, 'figures/efficiency_ZZ.fig');
+  saveas(fig, 'figures/efficiency_ZZ.fig');
   set(fig, 'Color', 'none'); % Set background transparent
-  export_fig(fig, 'figures/efficiency_zz','-pdf', '-painters'); % Export to PDF using export_fig
+  export_fig(fig, 'figures/efficiency_ZZ','-pdf', '-painters'); % Export to PDF using export_fig
 end
